35956904bfc5f2b2c12585d5f3c9d61be2e04213
Shinji Pons <accounts@shinjipons.com>
2024-02-09 17:52:30 +0100

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <nav className="w-full relative flex items-center justify-between max-w-3xl mx-auto px-4 py-5">
             <Link href="/" className="font-bold text-3xl">
-                Marshal<span className="text-primary">Blog</span>
+                MyDumb<span className="text-primary">Blog</span>
             </Link>
 
             <ModeToggle />
